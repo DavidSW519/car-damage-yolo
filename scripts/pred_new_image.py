b29adcefab47a4fd1741104f77d5cd59a52516db
Ángel David Figueroa Contreras <136935097+DavidSW519@users.noreply.github.com>
2026-02-18 21:39:44 -0600

    Pred using model

---
+++ scripts/pred_new_image.py
@@ -0,0 +1,19 @@
+import sys
+from ultralytics import YOLO
+import scripts.register_cardd_blocks 
+
+weights = "/content/car-damage-yolo/runs/detect/runs/cardd_backbone_compare_fast/A_resnetlike_lite_150epochs/weights/best.pt"
+
+img_path = "data/miata.jpg"
+model = YOLO(weights)
+
+results = model.predict(
+    source=img_path,
+    imgsz=512,
+    conf=0.25,
+    iou=0.5,
+    save=True,
+    show_labels=True,
+    show_conf=True
+)
+print("pred ok in", results[0].save_dir)
